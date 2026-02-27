3dd6849ff8a9d3c361e2293888f0c99b9cdae4a4
ceca27 <cecanabilaputri@gmail.comm>
2026-02-27 11:44:31 +0700

    bakso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ceca</title>
+        <title>Ceca
+            
+        </title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
